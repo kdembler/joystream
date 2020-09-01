b776dd43ccd0db80c18404e832a08a353cf95447
Klaudiusz Dembler <kdembler@gmail.com>
2020-09-01 16:22:37 +0200

    fix App test

--- atlas/packages/app/src/App.tsx
+++ atlas/packages/app/src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Router } from '@reach/router'
 
 import store from './store'
-import { Layout } from './components'
-import { HomeView, VideoView } from './views'
-import routes from './config/routes'
+import { LayoutWithRouting } from './components'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Layout>
-        <Router primary={false}>
-          <HomeView default />
-          <VideoView path={routes.video} />
-        </Router>
-      </Layout>
+      <LayoutWithRouting />
     </Provider>
   )
 }
